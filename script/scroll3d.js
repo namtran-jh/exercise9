e91a506579108917bcc4ffb0525c478f31b33241
thnammm <nam.tran@journeyh.io>
2020-08-10 15:41:03 +0700

    update scroll3d more flexible

--- script/scroll3d.js
+++ script/scroll3d.js
@@ -5,31 +5,38 @@ const UP = false;
 const DOWN = true;
 const direction = { type: null };
 
-document.addEventListener("scroll", function() {
-    let scroll = window.pageYOffset;
+// variables for clientHeight and offsetHeight of document
+const limit = document.getElementById("masthead1").offsetHeight + document.getElementById("cardScroller1").offsetHeight;
 
-    let initialPosition = document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY;
+if (window.pageYOffset > limit) document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY = `-500px`;
 
-    if (initialPosition === "") initialPosition = "0px";
-    // console.log({ scroll, initialPosition, lastScrollTop });
+if (document.documentElement.clientWidth > 1200) {
+    document.addEventListener("scroll", function() {
+        let scroll = window.pageYOffset;
 
-    if ((scroll - lastScrollTop) >= 5) {
-        lastScrollTop = scroll;
-        direction.type = DOWN;
-    }
+        let initialPosition = document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY;
 
-    if ((scroll - lastScrollTop) <= -5) {
-        lastScrollTop = scroll;
-        direction.type = UP;
-    }
+        if (initialPosition === "") initialPosition = "0px";
+        // console.log({ scroll, initialPosition, lastScrollTop });
 
-    if (direction.type !== null) {
-        if (direction.type) {
-            document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY = `${parseInt(initialPosition)-1}px`;
-            direction.type = null;
-        } else {
-            document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY = `${parseInt(initialPosition)+1}px`;
-            direction.type = null;
+        if ((scroll - lastScrollTop) >= 5) {
+            lastScrollTop = scroll;
+            direction.type = DOWN;
         }
-    }
-}, false);
+
+        if ((scroll - lastScrollTop) <= -5) {
+            lastScrollTop = scroll;
+            direction.type = UP;
+        }
+
+        if (direction.type !== null) {
+            if (direction.type) {
+                document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY = `${parseInt(initialPosition)-1}px`;
+                direction.type = null;
+            } else {
+                document.getElementsByClassName("masthead2Main")[0].style.backgroundPositionY = `${parseInt(initialPosition)+1}px`;
+                direction.type = null;
+            }
+        }
+    }, false);
+}
